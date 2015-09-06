1e8e55f1c432466d98d541cc6ce1d43f3cef7c96
Dane Stuckel <dane.stuckel@gmail.com>
2015-09-06 14:06:19 -0400

    added fig 3.8 and 3.9

---
+++ examples/fig_3-9.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt
+
+test_1_grades = [99, 90, 85, 97, 89]
+test_2_grades = [100, 85, 60, 90, 70]
+
+plt.scatter(test_1_grades, test_2_grades)
+plt.title('Axes aren\'t comparable')
+plt.xlabel('test 1 grades')
+plt.ylabel('test 2 grades')
+plt.axis('equal')
+plt.show()
